66c3f2bdd312afe2b3f296999681e407473a0b72
Felipe Luiz Carnevali <felipe.carnevali@hotmail.com>
2018-04-02 21:43:30 +1000

    Setup and create the first test

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.route('/books')
+.get((req, res) => {
+    res.json([{
+        id: 1, name: "Default Book"
+    }]);
+});
+
+export default app;
